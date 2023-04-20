955982b7a361d8172ba2f5c5583a838029638469
Laura Kimberly <lauraksp@icloud.com>
2023-04-20 12:11:13 -0300

    ajuste rota pedidos

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express(); // instancia do express
 
 const rotaProdutos = require('./routes/produtos'); // define variavel c diretorio de rota
+const rotaPedidos = require('./routes/pedidos'); // define variavel c diretorio de rota
+
 
 app.use('/produtos', rotaProdutos); // chama a rota produtos
+app.use('/pedidos', rotaPedidos); // chama a rota produtos
 
 module.exports = app; // exporta app 
 
