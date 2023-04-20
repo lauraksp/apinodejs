d1daddc82fd614411ff9269e0a204680c66604cc
Laura Kimberly <lauraksp@icloud.com>
2023-04-19 21:44:06 -0300

    pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -12,24 +12,31 @@ router.get('/', (req, res, next) => {
 // cria pedido
 router.post('/', (req, res, next) => {
     res.status(201).send({
-        mensagem: 'Pedido criado'
+        mensagem: 'Pedido'
     })    
 });
 
 
 // retorna os dados de um pedido
 router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido  
-    res.status(200).send({
-        mensagem: 'Voce passou um ID',
-        id_pedido: id
-    });
+    const id = req.params.id_produto
+
+    if (id === 'especial') {
+        res.status(200).send({
+            mensagem: 'Esse e o id especial',
+            id: id
+        });
+    } else {
+        res.status(200).send({
+            mensagem: 'Voce passou um ID'
+        });
+    }
 });
 
-// delete um pedido
+// deleta um pedido
 router.delete('/', (req, res, next) => {
     res.status(201).send({
-        mensagem: 'Utilizando o DELETE dentro da rota de pedido'
+        mensagem: 'Pedido excluido'
     })    
 });
 
